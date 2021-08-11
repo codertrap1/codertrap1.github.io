226c4936cfde9cd27cae8fd0b716e43851d3849c
codertrap1 <abdullahmian3055540@gmail.com>
2021-08-11 14:37:48 +0500

    s

--- rtcdemo/demo.js
+++ rtcdemo/demo.js
@@ -15,7 +15,7 @@ var Demo = (function () {
 
     // Set up the SignalR connection
     var hub = $.connection.webRtcHub;
-    $.connection.hub.url = 'http://202.166.161.157:8099/signalr/hubs';
+    $.connection.hub.url = 'https://202.166.161.157/signalr/hubs';
     $.connection.hub.start(function () {
         console.log('connected to signal server.');
     });
